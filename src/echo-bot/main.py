4336ca3151609884a7f5696433c9285413b3a791
Nikita Bormotov <pythonkaru@gmail.com>
2025-09-03 12:17:31 +0300

    Deleted decorations and added registers in main.py

--- src/echo-bot/main.py
+++ src/echo-bot/main.py
@@ -10,12 +10,10 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands="start"))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь и я отвечу.')
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands="help"))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -24,9 +22,13 @@ async def process_help_command(message: Message):
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
