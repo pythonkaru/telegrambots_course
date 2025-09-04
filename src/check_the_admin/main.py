089dbe8b4eb77940a5607f569bc31c751ef4fd0a
Nikita Bormotov <pythonkaru@gmail.com>
2025-09-04 17:02:03 +0300

    Added searching numbers in main.py

--- src/check_the_admin/main.py
+++ src/check_the_admin/main.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
@@ -10,8 +10,8 @@ BOT_TOKEN =  'BOT TOKEN HERE'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# Список с ID администраторов бота. !!!Замените на ваш!!!
-admin_ids = list[int] = []
+# Список с ID администраторов бота. !!!Замените 1111111 на ваш!!!
+admin_ids : list[int] = [1111111]
 
 # Собственный фильтр, проверяющий юзера на админа
 class IsAdmin(BaseFilter):
@@ -21,6 +21,41 @@ class IsAdmin(BaseFilter):
 
     async def __call__(self, message: Message) -> bool:
         return message.from_user.id in self.admin_ids
+# Этот фильтр будет проверять наличие неотрицательных чисел
+# в сообщении от пользователя, и передавать в хэндлер их список
+class NumbersInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        # Разрезаем сообщение по пробелам, нормализуем каждую часть, удаляя
+        # лишние знаки препинания и невидимые символы, проверяем на то, что
+        # в таких словах только цифры, приводим к целым числам
+        # и добавляем их в список
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',','').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+        # Если в списке есть числа - возвращаем словарь со списком чисел по ключу 'numbers'
+        if numbers:
+            return {'numbers': numbers}
+        return False
+
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа" и в нем есть числа
+@dp.message(F.text.lower().startswith('найди числа'), NumbersInMessage())
+# Помимо объекта типа Message, принимаем в хэндлер список чисел из фильтра
+# по соответствующему ключу `numbers`
+async def process_if_numbers(message: Message, numbers: list[int]):
+    await message.answer(
+        text=f'Нашел: {", ".join(str(num) for num in numbers)}'
+    )
+
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа", но в нем нет чисел
+@dp.message(F.text.lower().startswith('найди числа'))
+async def process_if_not_numbers(message: Message):
+    await message.answer(
+        text='Не нашел что-то :('
+    )
 
 # Этот хэндлер будет срабатывать, если апдейт от админа
 @dp.message(IsAdmin(admin_ids))
